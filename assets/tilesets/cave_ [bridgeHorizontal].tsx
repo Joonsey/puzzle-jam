d3c631a6b9c30cec17c44e4c7cbf28efd21b1c73
Johannes <johannes.ronning@outlook.com>
2024-06-29 03:18:31 +0200

    added assets and start of cave map

---
+++ assets/tilesets/cave_ [bridgeHorizontal].tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="cave_ [bridgeHorizontal]" tilewidth="16" tileheight="16" tilecount="72" columns="12">
+ <image source="../fantasy_ [version 2.0]/cave_/cave_ [bridgeHorizontal].png" width="192" height="96"/>
+</tileset>
